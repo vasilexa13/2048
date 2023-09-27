6257e37dffb6c22b6e1a5ea16e6af59cfd867856
vasilexa13 <vasilexa@gmail.com>
2023-09-27 21:39:39 +0300

    no message

--- src/Components/FieldComponent.js
+++ src/Components/FieldComponent.js
@@ -57,82 +57,88 @@ function Field(props) {
             if (event.keyCode == keysArr.right) {
                 keyRightMove();
                 function keyRightMove() {
-                    for (let coll = 0; coll < 4; coll++) {
-                        let emptyElement = [];
-                        let filledElement = [];
-                        let comparedElement;
-                        for (let x = 3; x >= 0; x--) {
-                            let element;
-                            if (nextSquares[coll][x] == null) {
-                                emptyElement.push(x);
-                            }
-                            else {
-                                filledElement.push(x);
-                                element = [coll, x];
-                                if (filledElement.length) {
-                                    if (emptyElement.length) {
+                    for (let x = 0; x < 4; x++) {
+                        for (let y = 3; x >= 0; x--) {
 
-                                        if ((filledElement.length > 1) && (nextSquares[coll][x] == nextSquares[coll][filledElement[0]])) {//filledElement[0] НЕ ТАК!
-                                            console.log('nextSquares', nextSquares[coll]);
-                                            console.log('coll-', coll, 'x-', x);
-
-                                            nextSquares[coll][filledElement[0]] = nextSquares[coll][filledElement[0]] + nextSquares[coll][filledElement[1]];//ПЕРЕСМОТРЕТЬ
-                                            nextSquares[coll][filledElement[1]] = null;
-                                        } else {
-                                            nextSquares[coll][emptyElement[0]] = nextSquares[coll][x];
-                                            nextSquares[coll][x] = null;
-                                        }
-                                        filledElement.pop();
-                                        filledElement.push(emptyElement[0]);
-
-                                        emptyElement.shift();
-                                        emptyElement.push(x);
-                                    }
-                                }
-                            }
-                            element = undefined;
                         }
-                        emptyElement = null;
-                        filledElement = null;
                     }
+                    //             for (let coll = 0; coll < 4; coll++) {
+                    //                 let emptyElement = [];
+                    //                 let filledElement = [];
+                    //                 let comparedElement;
+                    //                 for (let x = 3; x >= 0; x--) {
+                    //                     let element;
+                    //                     if (nextSquares[coll][x] == null) {
+                    //                         emptyElement.push(x);
+                    //                     }
+                    //                     else {
+                    //                         filledElement.push(x);
+                    //                         element = [coll, x];
+                    //                         if (filledElement.length) {
+                    //                             if (emptyElement.length) {
+
+                    //                                 if ((filledElement.length > 1) && (nextSquares[coll][x] == nextSquares[coll][filledElement[0]])) {//filledElement[0] НЕ ТАК!filledElement[filledElement.lenth-1]
+                    //                                     console.log('nextSquares', nextSquares[coll]);
+                    //                                     console.log('coll-', coll, 'x-', x);
+
+                    //                                     // nextSquares[coll][filledElement[0]] = nextSquares[coll][filledElement[0]] + nextSquares[coll][filledElement[1]];//ПЕРЕСМОТРЕТЬ
+                    //                                     nextSquares[coll][filledElement[0]] = nextSquares[coll][filledElement[0]] + nextSquares[coll][filledElement[1]];//ПЕРЕСМОТРЕТЬ
+                    //                                     nextSquares[coll][filledElement[1]] = null;
+                    //                                 } else {
+                    //                                     nextSquares[coll][emptyElement[0]] = nextSquares[coll][x];
+                    //                                     nextSquares[coll][x] = null;
+                    //                                 }
+                    //                                 filledElement.pop();
+                    //                                 filledElement.push(emptyElement[0]);
+
+                    //                                 emptyElement.shift();
+                    //                                 emptyElement.push(x);
+                    //                             }
+                    //                         }
+                    //                     }
+                    //                     element = undefined;
+                    //                 }
+                    //                 emptyElement = null;
+                    //                 filledElement = null;
+                    //             }
+                    //         }
+
+                    //     }
                 }
+                moveKey();
 
+                ///////////
             }
+
+            let fieldSize = +props.fieldSize;//приём размеров поля через props
+            fieldSize = 4;
+
+            let arrRow = [];
+            let arrField = [];
+            for (let i = 0; i < fieldSize; i++) {
+                for (let j = 0; j < fieldSize; j++) {
+                    arrRow[j] = <Square key={`${i}${j}`} value={squares[i][j]} onSquareClick={(event) => handleKey(randomIndex(), randomIndex(), event)
+                    }></Square >
+                }
+                arrField.push(arrRow);
+                arrRow = [];
+            }
+            // console.log(arrField);
+            return (
+
+                <div className="field" >
+                    <React.Fragment >
+                        {arrField}
+                    </React.Fragment>
+                </div>
+            );
+
         }
-        moveKey();
-
-        ///////////
-    }
-
-    let fieldSize = +props.fieldSize;//приём размеров поля через props
-    fieldSize = 4;
-
-    let arrRow = [];
-    let arrField = [];
-    for (let i = 0; i < fieldSize; i++) {
-        for (let j = 0; j < fieldSize; j++) {
-            arrRow[j] = <Square key={`${i}${j}`} value={squares[i][j]} onSquareClick={(event) => handleKey(randomIndex(), randomIndex(), event)
-            }></Square >
-        }
-        arrField.push(arrRow);
-        arrRow = [];
-    }
-    // console.log(arrField);
-    return (
-
-        <div className="field" >
-            <React.Fragment >
-                {arrField}
-            </React.Fragment>
-        </div>
-    );
-
-}
 
 
-export const arrField = () => arrField;
+        export const arrField = () => arrField;
 
-export default Field;
+        export default Field;
 
 
 
